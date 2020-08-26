5fa09530595516dfdf33a3144f81cadb3b5e580e
liambloom <liamrbloom@gmail.com>
2020-08-26 00:47:20 -0400

    Minor improvements to markup macro
    made name lower case, made less arms by using optional paramiter $()? for children, which also added the ability for normal tag syntax on non-parent tags

--- src/macros.rs
+++ src/macros.rs
@@ -1,23 +1,13 @@
 #[macro_export]
-macro_rules! Markup {
-    (<$tag_name:ident $($attr_name:ident=($attr_value:expr))*/>) => {
-        paste! {
-            [<$tag_name:camel>]::new(
-                [<$tag_name:camel Config>] {
-                    $($attr_name: $attr_value,)*
-                    ..[<$tag_name:camel Config>]::default()
-                }
-            )
-        }
-    };
-    (<$tag_name:ident $($attr_name:ident=($attr_value:expr))*> {$($child:tt)*} </$end_tag_name:ident>) => {{
+macro_rules! markup {
+    // Normal tag, optionally with children, which must be encased in curly braces ({})
+    (<$tag_name:ident $($attr_name:ident=($attr_value:expr))*> $({$($child:tt)*})? </$end_tag_name:ident>) => {{
         if (stringify!($tag_name) == stringify!($end_tag_name)) {
-            //println!("{}", stringify!(SGML!($child)));
             paste! {
                 [<$tag_name:camel>]::new(
                     [<$tag_name:camel Config>] {
                         $($attr_name: $attr_value,)*
-                        children: vec![$(Box::new(Markup!($child)),)*],
+                        $(children: vec![$(Box::new(markup!($child)),)*],)?
                         ..[<$tag_name:camel Config>]::default()
                     }
                 )
@@ -27,18 +17,16 @@ macro_rules! Markup {
             panic!("The begin and end tags must match");
         }
     }};
-    ({<$tag_name:ident $($attr_name:ident=($attr_value:expr))*> {$child:tt} </$end_tag_name:ident>}) => {
-        Markup!(<$tag_name $($attr_name=($attr_value))*> {$child} </$end_tag_name>)
+    // Self closing tag
+    (<$tag_name:ident $($attr_name:ident=($attr_value:expr))*/>) => {
+        markup!(<$tag_name $($attr_name=($attr_value))*></$tag_name>)
     };
+    // Tag with delimiter
+    ({<$tag_name:ident $($attr_name:ident=($attr_value:expr))*> $({$($child:tt)*})? </$end_tag_name:ident>}) => {
+        markup!(<$tag_name $($attr_name=($attr_value))*> $({$($child)*})? </$end_tag_name>)
+    };
+    // Self closing tag with delimiter
     ({<$tag_name:ident $($attr_name:ident=($attr_value:expr))*/>}) => {
-        Markup!(<$tag_name $($attr_name=($attr_value))*/>)
+        markup!(<$tag_name $($attr_name=($attr_value))*/>)
     };
 }
-
-/*#[macro_export]
-macro_rules! remove_delimiter {
-    (($($t:tt)*)) => {$($t)*};
-    ([$($t:tt)*]) => {$($t)*};
-    ({$($t:tt)*}) => {$($t)*};
-    ($($t:tt)*) => {$($t)*};
-}*/
