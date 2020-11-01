90b57faf34710769da5b9aa062f95951db8767cd
liambloom <liamrbloom@gmail.com>
2020-11-01 13:54:01 -0500

    Removed Ref downcasting methods because I no longer use Ref

--- graphic_cli/src/element_traits.rs
+++ graphic_cli/src/element_traits.rs
@@ -1,18 +1,12 @@
 use std::{
     rc::Rc,
-    cell::{Ref, RefMut, RefCell},
-    any::TypeId,
+    cell::RefCell,
     sync::atomic::AtomicUsize,
-    ops::Deref,
 };
 use crate::{
     elements::*,
 };
 use as_any_min::AsAny;
-
-fn type_id_of_value<T: 'static + ?Sized>(_value: &T) -> TypeId {
-    TypeId::of::<T>()
-}
 
 pub trait Element: AsElement + AsParent + AsChild + AsAny + 'static {
     fn id(&self) -> usize;
@@ -20,28 +14,6 @@ pub trait Element: AsElement + AsParent + AsChild + AsAny + 'static {
 
 impl dyn Element {
     const ELEMENT_COUNT: AtomicUsize = AtomicUsize::new(0);
-
-    fn downcast_ref<'a, T: Element>(el_ref: Ref<'a, dyn Element>) -> Result<Ref<'a, T>, Ref<'a, dyn Element>> {
-        if (*el_ref).as_any().is::<T>() {
-            // SAFETY: I checked that it is T, so it's safe
-            // The unsafe conversion is copied strait from the source code of the Any struct
-            Ok(Ref::map(el_ref, |inner| unsafe { &*(inner as *const dyn Element as *const T) } ))
-        }
-        else {
-            Err(el_ref)
-        }
-    }
-
-    fn downcast_mut<'a, T: Element>(el_ref: RefMut<'a, dyn Element>) -> Result<RefMut<'a, T>, RefMut<'a, dyn Element>> {
-        if (*el_ref).as_any().is::<T>() {
-            // SAFETY: I checked that it is T, so it's safe
-            // The unsafe conversion is copied strait from the source code of the Any struct
-            Ok(RefMut::map(el_ref, |inner| unsafe { &mut *(inner as *mut dyn Element as *mut T) }))
-        }
-        else {
-            Err(el_ref)
-        }
-    }
 }
 
 pub trait AsElement {
@@ -64,7 +36,6 @@ pub trait AsParent {
     fn is_parent(&self) -> bool;
     fn as_parent(&self) -> Option<&dyn Parent>;
     fn as_parent_mut(&mut self) -> Option<&mut dyn Parent>;
-    // fn as_parent_boxed(&self) -> Box<dyn Parent>;
 }
 
 pub trait Parent: Element {
@@ -72,15 +43,6 @@ pub trait Parent: Element {
 }
 
 impl dyn Parent {
-    fn ref_as_parent<'a>(el_ref: Ref<'a, dyn Element>) -> Option<Ref<'a, dyn Parent>> {
-        if el_ref.is_parent() {
-            Some(Ref::map(el_ref, |orig| orig.as_parent().unwrap()))
-        }
-        else {
-            None
-        }
-    }
-
     pub fn get_child(&self, id: usize) -> Option<Rc<RefCell<dyn Child>>> {
         for child in self.children().iter() {
             let child_ref = child.borrow();
@@ -88,7 +50,7 @@ impl dyn Parent {
                 return Some(Rc::clone(child))
             }
             else {
-                if let Some(child) = Parent::ref_as_parent(Ref::map(child_ref, |child| child.as_element())) {
+                if let Some(child) = child_ref.as_parent() {
                     let sub_child = child.get_child(id);
                     if sub_child.is_some() {
                         return sub_child;
